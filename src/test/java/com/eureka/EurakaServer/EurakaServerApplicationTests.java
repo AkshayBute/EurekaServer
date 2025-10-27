823daa67dc3f62dd8c08f9e73501ac0dab8a0d35
AkshayBute <buteakshay@gmail.com>
2025-10-27 23:26:49 +0530

    Eureka Server

---
+++ src/test/java/com/eureka/EurakaServer/EurakaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.eureka.EurakaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
